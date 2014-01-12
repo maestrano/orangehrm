529da71127034ddc849ef60a1be5189c4d6629cf
Arnaud Lachaume <arnaud.lachaume@welwire.com>
2014-01-12 16:45:21 +1100

    initial commit

---
+++ symfony/apps/orangehrm/templates/_footer_copyright_social_links.php
@@ -0,0 +1,18 @@
+<?php
+$imagePath = theme_path("images/login");
+?>
+<div id="footer" >
+    <div>
+    <?php include_partial('global/copyright'); ?>
+    </div>
+    <div id="social-icons">
+        <a href="http://www.linkedin.com/groups?home=&gid=891077" target="_blank">
+            <img src="<?php echo "{$imagePath}/linkedin.png"; ?>" /></a>&nbsp;
+        <a href="http://www.facebook.com/OrangeHRM" target="_blank">
+            <img src="<?php echo "{$imagePath}/facebook.png"; ?>" /></a>&nbsp;
+        <a href="http://twitter.com/orangehrm" target="_blank">
+            <img src="<?php echo "{$imagePath}/twiter.png"; ?>" /></a>&nbsp;
+        <a href="http://www.youtube.com/results?search_query=orangehrm&search_type=" target="_blank">
+            <img src="<?php echo "{$imagePath}/youtube.png"; ?>" /></a>&nbsp;
+    </div>
+</div>
---
+++ symfony/web/resource_dir_inc.php
@@ -0,0 +1,5 @@
+<?php 
+sfConfig::set('sf_web_css_dir_name','webres_521db8499614c0.51100681/css');
+sfConfig::set('sf_web_js_dir_name','webres_521db8499614c0.51100681/js');
+sfConfig::set('sf_web_images_dir_name','webres_521db8499614c0.51100681/images');
+sfConfig::set('ohrm_resource_dir', 'webres_521db8499614c0.51100681');
