4f84da50ac18e946cfd2cc7217711e61a112fc5e
Arnaud Lachaume <arnaud.lachaume@welwire.com>
2014-01-13 11:11:55 +1100

    configured autoloader for tests

--- maestrano/app/tests/bootstrap.php
+++ maestrano/app/tests/bootstrap.php
@@ -13,10 +13,25 @@ require PHP_SAML_DIR . 'XmlSec.php';
 define('MNO_PHP_SSO_DIR', './../../lib/mno-php/src/sso/');
 require MNO_PHP_SSO_DIR . 'MnoSsoBaseUser.php';
 
+// Define Autoload manager
+define('AUTOLOAD_MGT_DIR', './../..//lib/autoload-manager/');
+require AUTOLOAD_MGT_DIR . 'autoloadManager.php';
+
+$autoloadManager = new AutoloadManager();
+$autoloadManager->setSaveFile('./_autoload_hash_map.php');
+
 // Dependencies: your app files
 define('MY_APP_DIR', './../../../');
-require MY_APP_DIR . 'symfony/lib/vendor/symfony/lib/form/sfForm.class.php';
-require MY_APP_DIR . 'symfony/plugins/orangehrmPimPlugin/lib/form/AddEmployeeForm.php';
+$autoloadManager->addFolder(MY_APP_DIR . 'symfony/');
+$autoloadManager->register();
+require MY_APP_DIR . 'symfony/lib/vendor/symfony/lib/helper/I18NHelper.php';
+// require MY_APP_DIR . 'symfony/lib/vendor/symfony/lib/widget/sfWidget.class.php';
+// require MY_APP_DIR . 'symfony/lib/vendor/symfony/lib/widget/sfWidgetForm.class.php';
+// require MY_APP_DIR . 'symfony/lib/vendor/symfony/lib/widget/sfWidgetFormSchema.class.php';
+// require MY_APP_DIR . 'symfony/lib/vendor/symfony/lib/validator/sfValidatorBase.class.php';
+// require MY_APP_DIR . 'symfony/lib/vendor/symfony/lib/validator/sfValidatorSchema.class.php';
+// require MY_APP_DIR . 'symfony/lib/vendor/symfony/lib/form/sfForm.class.php';
+// require MY_APP_DIR . 'symfony/plugins/orangehrmPimPlugin/lib/form/AddEmployeeForm.php';
 
 
 // Tested class: 
