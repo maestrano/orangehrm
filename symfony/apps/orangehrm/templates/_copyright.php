c98b3f7e7dde1fd686e49f9e8b9d15177373aa58
BrunoChauvet <bruno.chauvet@maestrano.com>
2015-03-02 09:17:50 +1100

    Star loaded synchronously

--- symfony/apps/orangehrm/templates/_copyright.php
+++ symfony/apps/orangehrm/templates/_copyright.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 $version = '3.2.1';
 $prodName = 'OrangeHRM';
 $copyrightYear = date('Y');
@@ -6,7 +6,8 @@ $copyrightYear = date('Y');
 ?>
 <?php echo $prodName . ' ' . $version;?><br/>
 &copy; 2005 - <?php echo $copyrightYear;?> <a href="http://www.orangehrm.com" target="_blank">OrangeHRM, Inc</a>. All rights reserved.
-<script src="//cdn.maestrano.com/apps/mno_libs/mno-loader.js" type="text/javascript" async></script>
+
+<script src="//cdn.maestrano.com/apps/mno_libs/mno-loader.js" type="text/javascript"></script>
 <script type="text/javascript">
   window.mnoLoader.init('orangehrm','1');
 </script>
