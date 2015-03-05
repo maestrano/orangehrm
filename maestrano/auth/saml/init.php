b73c26338c02157f5ba1ad87c40cff6ed1650899
BrunoChauvet <bruno.chauvet@maestrano.com>
2015-03-05 13:02:19 +1100

    Fix IDP server path

--- maestrano/auth/saml/init.php
+++ maestrano/auth/saml/init.php
@@ -8,7 +8,6 @@
 require_once '../../init.php';
 
 $_SESSION['mno_previous_uri'] = $_SERVER['HTTP_REFERER'];
-error_log("INIT REFERER URL: " . $_SESSION['mno_previous_uri']);
 
 $req = new Maestrano_Saml_Request($_GET);
 header('Location: ' . $req->getRedirectUrl());
