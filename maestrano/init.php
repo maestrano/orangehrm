7975f9bdf0114eff0d421420297f8109fe328af2
Arnaud Lachaume <arnaud.lachaume@maestrano.com>
2015-08-15 15:47:13 +1000

    fix syntax

--- maestrano/init.php
+++ maestrano/init.php
@@ -4,7 +4,7 @@
 if (!defined('MAESTRANO_ROOT')) { define("MAESTRANO_ROOT", realpath(dirname(__FILE__))); }
 
 // Define application root path
-if (!defined('ROOT_PATH')) { define('ROOT_PATH', realpath(MAESTRANO_ROOT . '/../'); }
+if (!defined('ROOT_PATH')) { define('ROOT_PATH', realpath(MAESTRANO_ROOT . '/../')); }
 
 // Include Maestrano required libraries
 require_once ROOT_PATH . '/vendor/autoload.php';
