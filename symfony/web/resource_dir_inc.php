51d350036bfe0cfcdbcf0a205a868841c311ddc6
bruno <bruno.chauvet@maestrano.com>
2016-01-08 16:35:08 +1100

    version 3.2.1

---
+++ symfony/web/resource_dir_inc.php
@@ -0,0 +1,5 @@
+<?php 
+sfConfig::set('sf_web_css_dir_name','webres_548adc8dc8ab35.89553686/css');
+sfConfig::set('sf_web_js_dir_name','webres_548adc8dc8ab35.89553686/js');
+sfConfig::set('sf_web_images_dir_name','webres_548adc8dc8ab35.89553686/images');
+sfConfig::set('ohrm_resource_dir', 'webres_548adc8dc8ab35.89553686');
